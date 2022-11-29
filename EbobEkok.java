fa9793b829f3848d1260f26883cdbbb2caf868e7
mehmetorl <memo.874@hotmail.com>
2022-11-30 00:12:16 +0300

    ebobekok

--- EbobEkok.java
+++ EbobEkok.java
@@ -9,42 +9,22 @@ public class EbobEkok {
         s1=ek.nextInt();
         System.out.println("İkinci Sayıyı Giriniz.:");
         s2=ek.nextInt();
-       if(s1<s2)
-           {
+
                while (s3<=s1)
                    {
 
                        if(s1%s3==0&&s2%s3==0)
                        {
                            ebob=s3;
-
-                           s3++;
-                       } else if (s1%s3!=0||s2%s3!=0) {
-                           s3++;
                        }
+                       s3++;
                    }
                System.out.println("ebob:"+ebob);
 
                ekok=(s1*s2)/ebob;
                System.out.println("ekok:"+ekok);
-           }
-       else if(s2<s1)
-       {
-           while (s3<=s2)
-           {
-               if(s1%s3==0 && s2%s3==0)
-               {
-                  ebob=s3;
 
-                   s3++;
-               } else if ((s1%s3!=0||s2%s3!=0)) {
-                   s3++;
-               }
-           }
-           System.out.println("Ebob:"+ebob);
-           ekok=(s1*s2)/ebob;
-           System.out.println("Ekok:"+ekok);
-       }
+
 
     }
 }
